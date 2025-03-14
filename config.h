e9067cd05847575678037f930449403a067c0b4c
Théotime <theotimequiret@gmail.com>
2025-03-14 17:19:04 +0100

    Add files via upload

---
+++ config.h
@@ -0,0 +1,23 @@
+// Parametres de l'affichage VGA si on veut le tracer
+
+#define VGA_WIDTH  80   // Largeur VGA (ex: 800)
+#define VGA_HEIGHT 60   // Hauteur VGA (ex: 600)
+#define NUM_FFT_POINTS 20  // Nombre de points FFT simulés
+
+#define BORDER_SIZE 2   // Épaisseur de la bordure qui est modifiable ici
+
+// couleur de l'axe 
+#define AXIS_COLOR_R 244
+#define AXIS_COLOR_G 237
+#define AXIS_COLOR_B 222
+// blanc cassé
+
+// couleur des points FFT
+#define FFT_COLOR_R 135
+#define FFT_COLOR_G 206
+#define FFT_COLOR_B 235
+// sky blue
+
+#define freq_echantillonage 1000 // à voir
+
+
